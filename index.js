e886835f96bea20c6863bcbcab648be7b901cc2c
pantho0 <panthomashreky6@gmail.com>
2023-11-09 16:45:00 +0600

    orders api secured with jwt token

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
+require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware 
-app.use(cors())
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}))
 app.use(express.json())
+app.use(cookieParser())
 
+
+// own middleware
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.token;
+  console.log('token in the middleware', token);
+  if(!token){
+    return res.status(401).send({message:"Unauthorized"})
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err,decoded)=>{
+    if(err){
+      res.status(401).send({message:"Unauthorized"})
+    }
+    req.user = decoded;
+    next()
+  })
+}
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -32,16 +55,36 @@ async function run() {
     const foodCollection = client.db("pizzanDB").collection("foods");
     const ordersCollection = client.db("pizzanDB").collection("orders");
 
+    // JSON WEB TOKEN
+    app.post("/jwt", async(req,res)=>{
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+      res.cookie('token', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none'
+      })
+      res.send(user)
+    })
+    // if logout
+    app.post("/logout", async(req,res)=>{
+      const user = req.body;
+      console.log('logging out the user', user);
+      res.clearCookie('token', {maxAge:0}).send({success:true})
+    })
+
+
     // To get all the foods 
-    app.get("/api/v1/foods", async(req, res)=> {
+    app.get("/api/v1/foods", async (req, res) => {
       let query = {}
-      if(req.query?.addedBy){
-        query ={addedBy : req.query.addedBy}
+      if (req.query?.addedBy) {
+        query = { addedBy: req.query.addedBy }
       }
       // Pagination 
       const page = Number(req.query.page); //Number obj for make the url string to number value
       const limit = Number(req.query.limit);
-      const skip = (page-1)*limit;
+      const skip = (page - 1) * limit;
       const result = await foodCollection.find(query).skip(skip).limit(limit).toArray();
       res.send(result)
 
@@ -49,22 +92,22 @@ async function run() {
 
     // To count total foods 
 
-    app.get("/api/v1/productcount", async(req, res) =>{
+    app.get("/api/v1/productcount", async (req, res) => {
       const result = await foodCollection.estimatedDocumentCount();
-      res.send({count: result})
+      res.send({ count: result })
     })
 
     // To get single food by id 
-    app.get("/api/v1/foods/:id", async(req, res)=> {
+    app.get("/api/v1/foods/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id : new ObjectId(id)}
+      const query = { _id: new ObjectId(id) }
       const result = await foodCollection.findOne(query);
       res.send(result)
     })
 
 
     // for add product api
-    app.post("/api/v1/addFood", async(req, res)=>{
+    app.post("/api/v1/addFood", async (req, res) => {
       const food = req.body;
       const result = await foodCollection.insertOne(food)
       console.log(result);
@@ -74,16 +117,21 @@ async function run() {
 
     // orders api
 
-    app.get("/api/v1/orders", async(req,res)=>{
+    app.get("/api/v1/orders", verifyToken, async(req, res) => {  
+      console.log('token owner info', req.user);
+      if(req?.user?.email !== req?.query?.email){
+        return res.status(403).send({message:'Forbidden'})
+        }
+              
       let query = {}
-      if(req.query?.email){
-        query ={email : req.query.email}
+      if (req.query?.email) {
+        query = { email: req.query.email }
       }
       const result = await ordersCollection.find(query).toArray();
       res.send(result)
     })
 
-    app.post("/api/v1/confirmPurchase", async(req,res)=> {
+    app.post("/api/v1/confirmPurchase", async (req, res) => {
       const order = req.body;
       const result = await ordersCollection.insertOne(order)
       console.log(result);
@@ -91,9 +139,9 @@ async function run() {
     })
 
     // Order Delete api 
-    app.delete("/api/v1/orders/:id", async(req, res)=>{
+    app.delete("/api/v1/orders/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id : new ObjectId(id)}
+      const query = { _id: new ObjectId(id) }
       const result = await ordersCollection.deleteOne(query)
       res.send(result)
     })
@@ -119,10 +167,10 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send("Pizzan Restaurant Server Running")
+  res.send("Pizzan Restaurant Server Running")
 })
 
 
 app.listen(port, () => {
-    console.log(`Pizzan is running on port ${port}`);
+  console.log(`Pizzan is running on port ${port}`);
 })
